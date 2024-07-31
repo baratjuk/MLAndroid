682412d600220ff40e6709f971ffe416f3201b4b
Blueforce Dev <blueforcedev@staturesoftware.com>
2024-07-31 19:23:33 +0300

    camera preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.google.mlkit:image-labeling:17.0.8")
+    implementation(libs.camera)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/ml/MainActivity.kt
+++ app/src/main/java/com/example/ml/MainActivity.kt
@@ -1,47 +1,164 @@
 package com.example.ml
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.ml.ui.theme.MLTheme
+import com.google.mlkit.vision.camera.CameraXSource
+import java.util.ArrayList
 
 class MainActivity : ComponentActivity() {
+    val TAG = "ML.MainActivity"
+    private val REQUIRED_RUNTIME_PERMISSIONS =
+        arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    private val PERMISSION_REQUESTS = 1
+
+    private var cameraXSource: CameraXSource? = null
+    private var previewView: PreviewView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!allRuntimePermissionsGranted()) {
+            getRuntimePermissions()
+        }
+
         enableEdgeToEdge()
         setContent {
+            val context = LocalContext.current
             MLTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    PreviewView(context).apply {
+//                        setBackgroundColor(Color.Green.toArgb())
+//                        layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+//                        scaleType = PreviewView.ScaleType.FILL_START
+//                        implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+//                    }
+                    CameraPreview()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun allRuntimePermissionsGranted(): Boolean {
+        for (permission in REQUIRED_RUNTIME_PERMISSIONS) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MLTheme {
-        Greeting("Android")
+    private fun getRuntimePermissions() {
+        val permissionsToRequest = ArrayList<String>()
+        for (permission in REQUIRED_RUNTIME_PERMISSIONS) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    permissionsToRequest.add(permission)
+                }
+            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                PERMISSION_REQUESTS
+            )
+        }
+    }
+
+    private fun isPermissionGranted(context: Context, permission: String): Boolean {
+        if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.i(TAG, "Permission granted: $permission")
+            return true
+        }
+        Log.i(TAG, "Permission NOT granted: $permission")
+        return false
+    }
+
+    @Composable
+    fun CameraPreview(
+        modifier: Modifier = Modifier,
+        cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
+        scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
+    ) {
+        val lifecycleOwner = LocalLifecycleOwner.current
+        AndroidView(
+            modifier = modifier,
+            factory = { context ->
+                val previewView = PreviewView(context).apply {
+                    this.scaleType = scaleType
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    // Preview is incorrectly scaled in Compose on some devices without this
+                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                }
+
+                val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
+                cameraProviderFuture.addListener({
+                    val cameraProvider = cameraProviderFuture.get()
+
+                    // Preview
+                    val preview = Preview.Builder()
+                        .build()
+                        .also {
+                            it.setSurfaceProvider(previewView.surfaceProvider)
+                        }
+
+                    try {
+                        // Must unbind the use-cases before rebinding them.
+                        cameraProvider.unbindAll()
+
+                        cameraProvider.bindToLifecycle(
+                            lifecycleOwner, cameraSelector, preview
+                        )
+                    } catch (exc: Exception) {
+                        Log.v(TAG, "Use case binding failed", exc)
+                    }
+                }, ContextCompat.getMainExecutor(context))
+
+                previewView
+            })
     }
 }
